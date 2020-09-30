f26e8aa0fb6764eeb04e55d2c23bb3743d6c6b23
Steven Topazio <sat@northpointprinting.com>
2020-09-30 15:29:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, data|
-    data.each do 
+    data.each do |holiday, value|
+      
 
 
 end
