7e6750386ce22aaf07271569f64c875245b06de2
Steven Topazio <sat@northpointprinting.com>
2020-09-30 15:30:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, data|
     data.each do |holiday, value|
-      
+      value.each do |supply|
+        if 
 
 
 end
