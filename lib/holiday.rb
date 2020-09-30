030bbed62a2ea40c3377fe2985a130aee9ff9f7e
Steven Topazio <sat@northpointprinting.com>
2020-09-30 15:03:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   
-  holiday_hash.each do 
+  holiday_hash[:winter]
 
 end
 
